16c848568ec50fbc274627e22269b81382d283a5
Leakbox258 <3033680748@qq.com>
2026-01-21 21:33:40 +0800

    update

--- loop_detection.sv
+++ loop_detection.sv
@@ -1,27 +1,28 @@
-`timescale 1ns / 100ps
-
-
 module loop_detection #(
-    VECTOR_SIZE = 16
+    parameter         [31:0] POL_MASK = 32'h0000_00C0,
+    parameter integer        POL_W    = 7,
+    parameter integer        DW       = 16
 ) (
-    input clk,
+    input [POL_W-1:0] pn_state_in,
 
-    input reset,
-    input clk_en,
-    input [VECTOR_SIZE-1:0] in,
-    output [VECTOR_SIZE-1:0] out
+    output [DW-1:0]    pn_generated,
+    output [DW+POL_W-1:0] pn_full_state_out
 );
 
-  slide_vector0 sv0 (
-      clk,
-      reset,
-      clk_en,
-      in[0]
-  );
+  wire [DW-1:0] pn;
+  wire [DW+POL_W-1:0] pn_full_state;
 
-  slide_vector1 sv1 (
-      in,
-      out
-  );
+  assign pn_full_state = {pn_state_in[POL_W-1:0], pn};
+
+
+  generate
+    genvar i;
+    for (i = 0; i < DW; i = i + 1) begin : pn_loop
+      assign pn[i] = ^(pn_full_state[i+:POL_W+1] & POL_MASK);
+    end
+  endgenerate
+
+  assign pn_generated = pn;
+  assign pn_full_state_out = pn_full_state;
 
 endmodule
