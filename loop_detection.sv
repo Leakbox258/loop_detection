224164a134452c98e076acb1d30504968ea5cb5e
Leakbox258 <3033680748@qq.com>
2026-01-21 22:51:26 +0800

    update

--- loop_detection.sv
+++ loop_detection.sv
@@ -1,7 +1,7 @@
 module loop_detection #(
-    parameter         [31:0] POL_MASK = 32'h0000_00C0,
-    parameter integer        POL_W    = 7,
-    parameter integer        DW       = 16
+    parameter         [7:0] POL_MASK = 8'hC0,
+    parameter integer       POL_W    = 7,
+    parameter integer       DW       = 16
 ) (
     input [POL_W-1:0] pn_state_in,
 
@@ -12,13 +12,21 @@ module loop_detection #(
   wire [DW-1:0] pn;
   wire [DW+POL_W-1:0] pn_full_state;
 
-  assign pn_full_state = {pn_state_in[POL_W-1:0], pn};
-
+  assign pn_full_state = {pn_state_in[POL_W-1:0], pn};  // {[6:0], [15:0]}
 
   generate
     genvar i;
     for (i = 0; i < DW; i = i + 1) begin : pn_loop
       assign pn[i] = ^(pn_full_state[i+:POL_W+1] & POL_MASK);
+
+      // due to speciality of ^(... & 8'hC0) when POL_MASK[0] != '1
+      //
+      // pn[0] = pn[7] ^ pn[6]
+      // pn[1] = pn[8] ^ pn[7]
+      // ...
+      // pn[9] = pn_state_in[0] ^ pn[15]
+      // pn[15:10] doesn't combine with other bits of pn
+
     end
   endgenerate
 
